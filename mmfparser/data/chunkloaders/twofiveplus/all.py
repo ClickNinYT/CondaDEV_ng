9984362c00357ea08e98a076b883d94f28a420b8
ClickNinYT <74685931+ClickNinYT@users.noreply.github.com>
2022-01-25 09:13:42 +0700

    More attempt

--- mmfparser/data/chunkloaders/twofiveplus/all.py
+++ mmfparser/data/chunkloaders/twofiveplus/all.py
@@ -1,30 +1,36 @@
 from mmfparser.loader import DataLoader
 from mmfparser.bytereader import ByteReader
 from mmfparser.data.chunkloaders import all
+from mmfparser.data.chunkloaders.common import _ObjectTypeMixin
 import sys
 
 sys.path.append('..\..\..\..')
 from misc import *
 
 class ObjectHeaders(DataLoader, _ObjectTypeMixin):
-    def init(self):
-        self.headers = None
+    headers = None
+    headers_count = None
+    start = None
+    end = None
+    current = None
+    prop = None
+    chunkSize = None
     
     def read(self, reader):
-        start = reader.tell()
-        end = start + reader.size()
-        headers_count = 0
-        headers = all.ObjectHeader(reader)
-        current = 0
-        while reader.tell() < end:
-            prop = all.ObjectHeader(reader)
+        self.start = reader.tell()
+        self.end = self.start + reader.size()
+        self.headers_count = 0
+        self.headers = all.ObjectHeader(reader)
+        self.current = 0
+        while reader.tell() < self.end:
+            self.prop = all.ObjectHeader(reader)
             log("Reading object headers...", 1)
-            prop.read(reader)
-            chunkSize = reader.readInt()
-            headers_count = current
-            log(str(headers_count), 1)
-            log(str(headers), 1)
-            current += 1
+            self.prop.read(reader)
+            self.chunkSize = reader.readInt()
+            self.headers_count = self.current
+            log(str(self.headers_count), 1)
+            log(str(self.headers), 1)
+            self.current += 1
     
     def write(self, reader):
         log("Warning: Not yet implemented!", 3)
@@ -32,48 +38,57 @@ class ObjectHeaders(DataLoader, _ObjectTypeMixin):
 
 
 class ObjectPropertyList(DataLoader, _ObjectTypeMixin):
-    def init(self):
-        self.Props = None
+    start = None
+    end = None
+    chunksize = None
+    Props_count = None
+    Props = None
+    current = None
+    prop = None
 
     def read(self, reader):
-        start = reader.tell()
-        end = start + reader.size()
-        chunksize = reader.readInt()
-        Props_count = 0
-        Props = all.ObjectProperties(reader)
-        current = 0
-        while reader.tell() < end:
-            prop = all.ObjectProperties(reader)
-            prop.read()
+        self.start = reader.tell()
+        self.end = self.start + reader.size()
+        self.chunksize = reader.readInt()
+        self.Props_count = 0
+        self.Props = all.ObjectProperties(reader)
+        self.current = 0
+        while reader.tell() < self.end:
+            self.prop = all.ObjectProperties(reader)
+            self.prop.read()
             log("Reading object properties...", 1)
-            Props_count = current
-            log(str(Props_count), 1)
-            log(str(Props), 1)
-            current += 1
+            self.Props_count = self.current
+            log(str(self.Props_count), 1)
+            log(str(self.Props), 1)
+            self.current += 1
 
     def write(self, reader):
         log("Warning: Not yet implemented!", 3)
         pass
 
 class ObjectNames(DataLoader, _ObjectTypeMixin):
+    start = None
+    end = None
+    Names_count = None
+    Names = None
+    name = None
+    current = None
+    
     def write(self, reader):
         log("Warning: Not yet implemented!", 3)
         pass
 
     def read(self, reader):
-        start = reader.tell()
-        end = start + reader.size()
-        Names_count = 0
-        current = 0
-        while reader.tell() < end:
-            name = reader.readString()
-            Names_count = current
-            Names = name
-            log(str(Names_count), 1)
-            log(str(Names), 1)
-            current += 1
-
-    def __init__(self, reader):
-        self.Names = None
+        self.start = reader.tell()
+        self.end = self.start + reader.size()
+        self.Names_count = 0
+        self.current = 0
+        while reader.tell() < self.end:
+            self.name = reader.readString()
+            self.Names_count = self.current
+            self.Names = self.name
+            log(str(self.Names_count), 1)
+            log(str(self.Names), 1)
+            self.current += 1
         
         
